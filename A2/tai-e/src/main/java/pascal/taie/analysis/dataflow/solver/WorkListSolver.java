7b6369e2791624fe190e371affa9c9b460fda7eb
z3ratu1 <1207039417@qq.com>
2022-11-02 22:59:37 +0800

    finish assignment2

--- A2/tai-e/src/main/java/pascal/taie/analysis/dataflow/solver/WorkListSolver.java
+++ A2/tai-e/src/main/java/pascal/taie/analysis/dataflow/solver/WorkListSolver.java
@@ -26,6 +26,9 @@ import pascal.taie.analysis.dataflow.analysis.DataflowAnalysis;
 import pascal.taie.analysis.dataflow.fact.DataflowResult;
 import pascal.taie.analysis.graph.cfg.CFG;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 class WorkListSolver<Node, Fact> extends Solver<Node, Fact> {
 
     WorkListSolver(DataflowAnalysis<Node, Fact> analysis) {
@@ -35,6 +38,19 @@ class WorkListSolver<Node, Fact> extends Solver<Node, Fact> {
     @Override
     protected void doSolveForward(CFG<Node> cfg, DataflowResult<Node, Fact> result) {
         // TODO - finish me
+        // worklist
+        Queue<Node> workList = new LinkedList<>(cfg.getNodes());
+        while (!workList.isEmpty()){
+            Node node = workList.poll();
+            Fact inFact = result.getInFact(node);
+            Fact outFact = result.getOutFact(node);
+            for (Node pred:cfg.getPredsOf(node)) {
+                analysis.meetInto(result.getOutFact(pred), inFact);
+            }
+            if(analysis.transferNode(node, inFact, outFact)){
+                workList.addAll(cfg.getSuccsOf(node));
+            }
+        }
     }
 
     @Override
